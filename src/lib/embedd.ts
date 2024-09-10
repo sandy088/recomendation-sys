bac82b3362d3a1b0768f23b5594cc97158ff8ef8
Sandeep <sandeeppakho55@gmail.com>
2024-09-10 12:22:53 +0530

    Custom Youtube Recomendation System

--- src/lib/embedd.ts
+++ src/lib/embedd.ts
@@ -1,7 +1,7 @@
 import { VoyageEmbeddings } from "@langchain/community/embeddings/voyage";
 
 const embeddings = new VoyageEmbeddings({
-  apiKey: "pa-onMc1WkJ5YHghHPdVbS00XNhtVe960RbA4B2OX1j3cc", // In Node.js defaults to process.env.VOYAGEAI_API_KEY
+  apiKey: "", // In Node.js defaults to process.env.VOYAGEAI_API_KEY
   inputType: "query", // Optional: specify input type as 'query', 'document', or omit for None / Undefined / Null
 });
 
